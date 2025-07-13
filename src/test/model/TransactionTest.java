2bd9809acf0ce6a1c9c2eedc3ea6f3bf49e6d104
ostenn <108330010+ostenn@users.noreply.github.com>
2025-07-12 17:45:35 -0700

    Added tests to Transaction class

--- src/test/model/TransactionTest.java
+++ src/test/model/TransactionTest.java
@@ -9,13 +9,19 @@ import org.junit.jupiter.api.Test;
 
 public class TransactionTest {
     
+    private Transaction transaction;
+    private LocalDate testDate;
     @BeforeEach
     void runBefore() {
-       
+        testDate = LocalDate.of(2025, 7, 12);
+        transaction = new Transaction(1000, "Bi-Weekly Salary", "Income", testDate);
     }
 
     @Test
     void testConstructor() {
-        
+        assertEquals(1000, transaction.getAmount());
+        assertEquals("Bi-Weekly Salary", transaction.getDescription());
+        assertEquals("Income", transaction.getCategory());
+        assertEquals(testDate, transaction.getDate());
     }
 }
